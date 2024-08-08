8335c3e6696da24c816aa9fde2e846b20b49dea8
CXu0607 <gx7@rice.edu>
2024-08-08 11:04:08 -0500

    Adding OAuth2

---
+++ src/EC3OAuth2/OAuth2Handler.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Diagnostics;
+using System.Collections.Specialized;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace WoodchuckCarbonTool.src.EC3OAuth2
+{
+    internal class TestOAuthHandler
+    {
+        private const string RedirectUri = "http://localhost:8000/callback";
+        private const string ClientId = "e4XzLm6W9s61J59Q5pRIoly58Q0EARp2ZJESaqYq";
+        private const string AuthorizationEndpoint = "https://buildingtransparency.org/oauth2/authorize";
+        private const string TokenEndpoint = "https://buildingtransparency.org/api/oauth2/token";
+
+        public static async Task<string> GetAccessTokenAsync()
+        {
+            string authorizationUrl = $"{AuthorizationEndpoint}?client_id={ClientId}&redirect_uri={Uri.EscapeDataString(RedirectUri)}&response_type=code&scope=read";
+
+            using (HttpListener listener = new HttpListener())
+            {
+                listener.Prefixes.Add(RedirectUri + "/");
+                listener.Start();
+
+                // Open the authorization URL in the user's default browser
+                Process.Start(new ProcessStartInfo { FileName = authorizationUrl, UseShellExecute = true });
+
+                // Wait for the authorization response
+                HttpListenerContext context = await listener.GetContextAsync();
+
+                // Extract the authorization code from the request
+                string authorizationCode = ExtractAuthorizationCode(context.Request.Url);
+
+                // Respond to the browser
+                byte[] responseBuffer = Encoding.UTF8.GetBytes("Authorization successful! You can close this window.");
+                context.Response.ContentLength64 = responseBuffer.Length;
+                context.Response.OutputStream.Write(responseBuffer, 0, responseBuffer.Length);
+                context.Response.OutputStream.Close();
+
+                // Exchange the authorization code for an access token
+                string accessToken = await ExchangeCodeForTokenAsync(authorizationCode);
+                return accessToken;
+            }
+        }
+
+        private static string ExtractAuthorizationCode(Uri uri)
+        {
+            NameValueCollection query = HttpUtility.ParseQueryString(uri.Query);
+            return query.Get("code");
+        }
+
+        private static async Task<string> ExchangeCodeForTokenAsync(string authorizationCode)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                var requestContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("grant_type", "authorization_code"),
+                    new KeyValuePair<string, string>("code", authorizationCode),
+                    new KeyValuePair<string, string>("redirect_uri", RedirectUri),
+                    new KeyValuePair<string, string>("client_id", ClientId)
+                });
+
+                HttpResponseMessage response = await client.PostAsync(TokenEndpoint, requestContent);
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                dynamic jsonResponse = JsonConvert.DeserializeObject(responseContent);
+                return jsonResponse.access_token;
+            }
+        }
+    }
+}
